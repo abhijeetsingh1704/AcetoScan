9dbaf88906beb56ad21bc81d8a6936049922b895
Johan Nylander <Johan.Nylander@nrm.se>
2019-05-07 16:25:48 +0200

    work in progress

--- SCRIPTS/Software_check.sh
+++ SCRIPTS/Software_check.sh
@@ -1,45 +1,49 @@
 #!/bin/bash
 
 # File: Software_check.sh
-# Last modified: tis maj 07, 2019  12:21
+# Last modified: tis maj 07, 2019  04:24
 # Sign: JN
 
 ### Checking if cutadapt is installed
 if ! command -v "cutadapt" > /dev/null ; then
-    echo "cutadapt not found, Aborting !!!"
+    echo "Error: cutadapt not found, Aborting !!!"
     exit 1
 fi
 
 ### Checking if vsearch is installed
 if ! command -v "vsearch" > /dev/null ; then
-    echo "vsearch not found, Aborting !!!"
+    echo "Error: vsearch not found, Aborting !!!"
     exit 1
 fi
 
 ### Checking if getorf from Emboss is installed
 if ! command -v "getorf" > /dev/null ;then
-    echo "EMBOSS not found, Aborting !!!"
+    echo "Error: EMBOSS not found, Aborting !!!"
     exit 1
 fi
 
 ### Checking if Blast is installed
 if ! command -v "blastx" > /dev/null ; then
-    echo "NCBI Blast+ (blastx) not found, Aborting !!!"
+    echo "Error: NCBI Blast+ (blastx) not found, Aborting !!!"
     exit 1
 fi
 
 ### Checking if R and Rscript are installed
 if ! command -v "R" > /dev/null ; then
-    echo "R not found, Aborting !!!"
+    echo "Error: R not found, Aborting !!!"
     exit 1
 fi
 if ! command -v "Rscript" > /dev/null ; then
-    echo "Rscript not found, Aborting !!!"
+    echo "Error: Rscript not found, Aborting !!!"
     exit 1
 fi
 
 ### Check if R packages are installed (ad hoc way)
-if Rscript --default-packages=phyloseq,ggplot2,plotly,RColorBrewer,randomcoloRrandomcoloRplyr,dplyr -e 'q()' &> /dev/null ; then
-    Rscript --default-packages=phyloseq,ggplot2,plotly,RColorBrewer,randomcoloR,plyr,dplyr -e 'q()' 2>&1 | grep 'not found'
-    exit 1
+
+ret=$(Rscript --default-packages=phyloseq,ggplot2,plotly,RColorBrewer,randomcoloR,plyr,dplyr -e 'q()' 2>&1 | grep -v 'Loading required package')
+if [ ! -z "$ret" ] ; then
+    echo "Error: Missing R package(s):"
+    echo "$ret"
+    exit
 fi
+
--- SCRIPTS/cutadapt_illumina.sh
+++ SCRIPTS/cutadapt_illumina.sh
@@ -1,35 +1,54 @@
 #!/bin/bash
 
-#Cutadapt script for cleaning ILLUMINA forward read data (R1), 
-#requires already demplutiplexed data in the form of 
-#SAMPLE_R1_001.fastq.gz. 
+# File: cutadapt_illumina.sh
+# Last modified: tis maj 07, 2019  01:27
+# Sign: JN
 
-#Script is only for forward reads.
+# Cutadapt script for cleaning ILLUMINA forward read data (R1), 
+# requires already demplutiplexed data in the form of 
+# SAMPLE_R1_001.fastq.gz. 
+
+# Script is only for forward reads.
 
 exec 3>&1 4>&2
 
 trap 'exec 2>&4 1>&3' 0 1 2 3
 
-exec 1>$CDIR/OUTPUT_DATA/cutadapt.out 2>&1
+exec 1> "${CDIR}/OUTPUT_DATA/cutadapt.out" 2>&1
 
-
-mkdir -p $CDIR/OUTPUT_DATA/trimmed
+# TODO: avoid creating directories by a worker script
+mkdir -p "${CDIR}/OUTPUT_DATA/trimmed"
 
 #module load cutadapt
 
+# TODO: Why can't we use $file directly?
+# Aren't $file == $FwdIn ? 
 for file in *_R1_001.fastq.gz; do
 
-prefix=${file%_R1_001.fastq.gz};
+    prefix="${file%_R1_001.fastq.gz}"
 
-FwdIn=${prefix}_R1_001.fastq.gz;
+    FwdIn="${prefix}_R1_001.fastq.gz"
 
-prefix="${file%%_*}"
+    prefix="${file%%_*}"
 
-FwdOut=${prefix}_trimmed_R1.fastq.gz
+    FwdOut="${prefix}_trimmed_R1.fastq.gz"
 
-#echo $FwdIn $FwdOut 
+    #echo $FwdIn $FwdOut 
 
-cutadapt -b CCNACNCCNNNNGGNGANGGNAA -b GGNTGNGGNNNNCCNCTNCCNTT -b ATNTTNGCNAANGGNCCNNCNTG -b TANAANCGNTTNCCNGGNNGNAC --max-n 0 --maximum-length 277 --minimum-length 150 --discard-untrimmed -j 0 -q 20 --length-tag "size=" -o $CDIR/OUTPUT_DATA/trimmed/${FwdOut} ${FwdIn};
+    cutadapt \
+        -b CCNACNCCNNNNGGNGANGGNAA \
+        -b GGNTGNGGNNNNCCNCTNCCNTT \
+        -b ATNTTNGCNAANGGNCCNNCNTG \
+        -b TANAANCGNTTNCCNGGNNGNAC \
+        --max-n 0 \
+        --maximum-length 277 \
+        --minimum-length 150 \
+        --discard-untrimmed \
+        -j 0 \
+        -q 20 \
+        --length-tag "size=" \
+        -o "${CDIR}/OUTPUT_DATA/trimmed/${FwdOut}" \
+        "${FwdIn}"
 
 done
 #module unload cutadapt
