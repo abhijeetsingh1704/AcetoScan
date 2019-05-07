b16891171a211219ced47cb1f2b845ec48e0f5c0
Johan Nylander <Johan.Nylander@nrm.se>
2019-05-07 11:02:57 +0200

    work in progress

--- SCRIPTS/Software_check.sh
+++ SCRIPTS/Software_check.sh
@@ -1,54 +1,45 @@
 #!/bin/bash
 
+# File: Software_check.sh
+# Last modified: tis maj 07, 2019  10:52
+# Sign: JN
+
 ###	Checking if cutadapt is installed
-
-if command -v "cutadapt" >/dev/null ;then
-        echo "" >/dev/null
-  else
-	echo ""	
-	echo "cutadapt not found, Aborting !!!" ; exit 1
-	echo ""
+if ! command -v "cutadapt" > /dev/null ;then
+	echo "cutadapt not found, Aborting !!!"
+    exit 1
 fi
-
 
 ###	Checking if vsearch is installed
-
-if command -v "vsearch" >/dev/null ;then
-        echo "" >/dev/null
-  else
-	echo ""	
-	echo "vsearch not found, Aborting !!!" ; exit 1
-	echo ""
+if command -v "vsearch" > /dev/null ;then
+	echo "vsearch not found, Aborting !!!"
+    exit 1
 fi
 
-###	Checking if Emboss is installed
-
-if command -v "getorf" >/dev/null ;then
-        echo "" >/dev/null
-  else
-	echo ""	
-	echo "EMBOSS not found, Aborting !!!" ; exit 1
-	echo ""
-fi
-
-
-###	Checking if Blast is installed
-
-if command -v "blastx" >/dev/null ;then
-	echo "" >/dev/null
-else
-	echo ""	
-	echo "Blast not found, Aborting !!!" && exit 1
-	echo ""
+###	Checking if getorf from Emboss is installed
+if ! command -v "getorf" > /dev/null ;then
+	echo "EMBOSS not found, Aborting !!!"
+    exit 1
 fi
 
 ###	Checking if Blast is installed
+if ! command -v "blastx" > /dev/null ; then
+    echo "NCBI Blast+ (blastx) not found, Aborting !!!"
+    exit 1
+fi
 
-if command -v "R" >/dev/null ;then
-	echo "Highly recommended to install required R libraries before proceeding !!!" 
-	sleep 2
-else
-	echo ""	
-	echo "R not found, Aborting !!!" && exit 1
-	echo ""
+###	Checking if R is installed
+if ! command -v "R" > /dev/null ;then
+	echo "R not found, Aborting !!!"
+    exit 1
+fi
+if ! command -v "Rscript" > /dev/null ;then
+	echo "Rscript not found, Aborting !!!"
+    exit 1
+fi
+
+### Check if R packages are installed (ad hoc way)
+if Rscript --default-packages=phyloseq,ggplot2,plotly,RColorBrewer,randomcoloRrandomcoloRplyr,dplyr -e 'q()' &> /dev/null ; then
+    Rscript --default-packages=phyloseq,ggplot2,plotly,RColorBrewer,randomcoloR,plyr,dplyr -e 'q()' 2>&1 | grep 'not found'
+    exit 1
 fi
