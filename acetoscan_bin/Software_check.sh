35d7ce6b39f7f162a1dc844ae61880c7fb2a2f22
Abhijeet Singh <22393643+abhijeetsingh1704@users.noreply.github.com>
2019-06-02 19:22:12 +0200

    Create Software_check.sh

---
+++ acetoscan_bin/Software_check.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# File: Software_check.sh
+# Last modified: mån maj 20, 2019  01:13
+# Sign: Abhi
+
+###     User
+
+user=`echo ${SUDO_USER:-${USER}}`
+
+###     Checking if cutadapt is installed
+if ! command -v "cutadapt" > /dev/null ; then
+    echo "Error: cutadapt not found, Aborting !!!"
+    exit 1
+fi
+
+###     Checking if vsearch is installed
+if ! command -v "vsearch" > /dev/null ; then
+    echo "Error: vsearch not found, Aborting !!!"
+    exit 1
+fi
+
+###     Checking if Blast is installed
+if ! command -v "blastx" > /dev/null ; then
+    echo "Error: NCBI Blast+ (blastx) not found, Aborting !!!"
+    exit 1
+fi
+
+###     Checking if R and Rscript are installed
+if ! command -v "R" > /dev/null ; then
+    echo "Error: R not found, Aborting !!!"
+    exit 1
+fi
+#
+if ! command -v "Rscript" > /dev/null ; then
+    echo "Error: Rscript not found, Aborting !!!"
+    exit 1
+fi
+
+###     Checking if acetobase is formatted and accessible
+
+if [ ! -f /home/$user/acetoscan/acetobase/*.phr ];then
+        echo -e "\n#Cannot access acetobase"
+        echo "Trying to Download AcetoBase"
+        sudo wget -P /home/$user/acetoscan/acetobase/ "https://www.acetobase.molbio.slu.se/path/to/AcetoBase_V1.tgz"
+        sudo tar xf /home/$user/acetoscan/acetobase/AcetoBase_V1.tgz -C /home/$user/acetoscan/acetobase/
+fi
+
+###     End of script
