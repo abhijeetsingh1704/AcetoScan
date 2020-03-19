ab9a86c2c4f34e2d0b3302f1eb0ca1f3e7e3635f
Abhijeet Singh <abhijeetsingh.aau@gmail.com>
2020-03-19 17:00:20 +0100

    Update AcetoScan_cutadapt.sh

--- acetoscan_bin/AcetoScan_cutadapt.sh
+++ acetoscan_bin/AcetoScan_cutadapt.sh
@@ -1,46 +1,48 @@
 #!/bin/bash
 
-# File: AcetoScan_cutadapt.sh
-# Last modified: Tor, Mar 16, 2020 17:18
-# Sign: Abhi
+#   File: AcetoScan_cutadapt.sh
+#   Last modified: Tor, Mar 19, 2020 10:00
+#   Sign: Abhi
 
-# Cutadapt script for cleaning ILLUMINA read data, 
-# requires already demplutiplexed data in the form of 
-# SAMPLE_R*_001.fastq.(gz|bz2).
+### Cutadapt script for cleaning ILLUMINA read data,
+### requires already demplutiplexed data in the form of
+### SAMPLE_R*_001.fastq.(gz|bz2).
 
-command -v cutadapt >/dev/null 2>&1 || { echo >&2 "Error: cutadapt not found."; exit 1; }
+### Check cutadapt program
 
-cutadapt_dir="${WKDIR}/output_data/trimmed"
+    command -v cutadapt >/dev/null 2>&1 || { echo >&2 "Error: cutadapt not found."; exit 1; }
 
-cutadapt_out="${WKDIR}/output_data/cutadapt.out"
+### Defining process variable
 
-echo "cutadapt $(date) :" > "${cutadapt_out}"
+    cutadapt_dir="${WKDIR}/output_data/trimmed"
+    cutadapt_out="${WKDIR}/output_data/cutadapt.out"
 
-for infile in $(find "${WKDIR}/input_data/" -maxdepth 1 -type l -name "*_${reads}_001.fastq.${INfileext}" ); do
+### Making cutadapt log file
 
-#for FwdIn in *"${reads}"*.fastq."${INfileext}"; do
+    echo "cutadapt $(date) :" > "${cutadapt_out}"
 
-  if [ ! -L "${infile}" ]; then
-        echo "Error: No *.fastq.'${INfileext}' files found in ${PWD}"
-        break
-    else
-    	In=`echo ${infile} | rev | cut -d "/" -f1 | cut -d. -f3,4,5,6,7,8,9,10 | rev | sed "s/_L001_${reads}_001//"`
-        FwdOut="${In}_trimmed_"${reads}".fasta"
-        cutadapt \
-            -b "CCNACNCCNNNNGGNGANGGNAA" \
-            -b "GGNTGNGGNNNNCCNCTNCCNTT" \
-            -b "ATNTTNGCNAANGGNCCNNCNTG" \
-            -b "TANAANCGNTTNCCNGGNNGNAC" \
-            --max-n 0 \
-            --maximum-length $MaxL \
-            --minimum-length $MinL \
-            --untrimmed-output ${WKDIR}/output_data/untrimmed \
-            -j 0 \
-            -q $QT \
-            --length-tag "size=" \
-            -o "${cutadapt_dir}/${FwdOut}" \
-            "${infile}" >> "${cutadapt_out}"
-    fi
-done
+### Find the file and execute the adapter trimming and quality filtering
+
+    for infile in $(find "${WKDIR}/input_data/" -maxdepth 1 -type l -name "*_${reads}_001.fastq.${INfileext}" ); do
+
+        if [ ! -L "${infile}" ]; then
+            echo "Error: No *.fastq.'${INfileext}' files found in ${cutinput}"
+            break
+        else
+            In="$(echo ${infile} | rev | cut -d "/" -f1 | cut -d. -f3,4,5,6,7,8,9,10 | rev | sed "s/_L001_${reads}_001//")"
+            FwdOut="${In}_trimmed_"${reads}".fasta"
+            cutadapt \
+                --max-n 0 \
+                --trim-n \
+                --maximum-length $MaxL \
+                --minimum-length $MinL \
+                -u 24 \
+                -j ${jthreads} \
+                -q $QT \
+                --length-tag "size=" \
+                -o "${cutadapt_dir}/${FwdOut}" \
+                "${infile}" >> "${cutadapt_out}"
+        fi
+    done
 
 ### End of script
